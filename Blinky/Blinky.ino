4d3037582f257c244100705c0d908b3ecc96b5f9
Unknown <caleb.washburn.3669@mail.linnbenton.edu>
2017-11-21 10:49:27 -0800

    Blinky works!

--- Blinky/Blinky.ino
+++ Blinky/Blinky.ino
@@ -1,9 +1,13 @@
-void setup() {
-  // put your setup code here, to run once:
+void setup(){
 
+  pinMode(13, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
 
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(500);
 }
+
