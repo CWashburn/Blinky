2e21cfee693af446402990a8e5467acac6372fdd
Unknown <caleb.washburn.3669@mail.linnbenton.edu>
2017-11-21 10:56:31 -0800

    Change blink pin to 12

--- Blinky/Blinky.ino
+++ Blinky/Blinky.ino
@@ -1,13 +1,13 @@
 void setup(){
 
-  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop(){
 
-  digitalWrite(13, HIGH);
-  delay(500);
-  digitalWrite(13, LOW);
-  delay(500);
+  digitalWrite(12, HIGH);
+  delay(50);
+  digitalWrite(12, LOW);
+  delay(50);
 }
 
